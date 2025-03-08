af57dcdd0b545a6c5ea3c21243839ec3ee207a39
Andrea Bettarini <bettarini@monema.it>
2025-03-09 00:38:36 +0100

    Aggiunto plugin pwa per gestire il serviceWorker

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,35 @@
 import react from '@vitejs/plugin-react'
 import { defineConfig, UserConfig } from 'vite'
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({ 
+      registerType: 'autoUpdate',
+      injectRegister: 'auto',
+      includeAssets: ['favicon.ico', 'apple-touch-icon.png', 'mask-icon.svg'],
+      manifest: {
+        name: 'TSN Lastra a Signa',
+        short_name: 'TSNLas',
+        description: 'Sito web del Tiro a Segno Nazionale di Lastra a Signa',
+        theme_color: '#ffffff',
+        icons: [
+          {
+            src: 'pwa-192x192.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: 'pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png'
+          }
+        ]
+      }
+    })
+  ],
   assetsInclude: ['**/*.png', '**/*.svg'],
 }) as UserConfig
+
